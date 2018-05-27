875b9d40e1318f40a178098bb5ac96a5c8fe7fa6
Artem Belkov <bestK1ngArthur@aol.com>
2018-05-27 19:18:34 +0300

    Initial Commit

---
+++ PlexSweeper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PlexSweeper
+//
+//  Created by Arthur K1ng on 27/05/2018.
+//  Copyright © 2018 Artem Belkov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PlexSweeper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PlexSweeper
+//
+//  Created by Arthur K1ng on 27/05/2018.
+//  Copyright © 2018 Artem Belkov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
