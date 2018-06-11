0f50ef0d53de54e10a1125c3ee995d3e402259f0
Artem Belkov <bestK1ngArthur@aol.com>
2018-06-11 23:02:03 +0300

    Transform to menu bar app

--- PlexSweeper/AppDelegate.swift
+++ PlexSweeper/AppDelegate.swift
@@ -11,16 +11,30 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
 
+        // Add status bar button
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("StatusBarImage"))
+            button.action = #selector(showWindow)
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    // MARK: - Window
+    
+    @objc func showWindow() {
+        
+        if let shownWindow = NSApplication.shared.windows.last {
+            shownWindow.close()
+        }
+        
+        let sweeperWindowController = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil).instantiateController(withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "MainWindowController")) as? NSWindowController
+        sweeperWindowController?.showWindow(self)
+    }
 }
-
